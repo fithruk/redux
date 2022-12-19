1d45c7c63096510d50a54322fe1a9a42e810274c
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-12-19 21:41:50 +0200

    2323

--- actionsWithPayload/src/index.js
+++ actionsWithPayload/src/index.js
@@ -1,5 +1,5 @@
-import store from "./store";
-import { addUser, deleteUser } from "./users.actions";
+import store from "./store.js";
+import { addUser, deleteUser } from "./users.actions.js";
 
 store.dispatch(addUser({ id: 76, name: "Sarah" }));
 store.dispatch(addUser({ id: 77, name: "Vasya" }));
