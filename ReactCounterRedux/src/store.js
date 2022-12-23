aaab4a30a2f6ed95b29298011b33386b54c6be56
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-12-23 15:08:43 +0200

    2323

--- ReactCounterRedux/src/store.js
+++ ReactCounterRedux/src/store.js
@@ -1,6 +1,14 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { counterReducer } from "./counterReducer";
 
-const store = createStore(counterReducer);
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.info("dispatching", action);
+  let result = next(action);
+  console.log("next state", store.getState());
+  console.groupEnd();
+};
+
+const store = createStore(counterReducer, applyMiddleware(logger));
 
 export default store;
